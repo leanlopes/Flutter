353022035b3b07ceb1c28920af3b4d6ddbd38d5b
leanlopes <tacodigamer@gmail.com>
2020-02-27 12:29:31 -0300

    first commit

---
+++ projetoteste/lib/main.dart
@@ -0,0 +1,14 @@
+'import 'package:flutter/material.dart';
+void main() => runApp(
+  MaterialApp(
+    home: Center(
+      child: Container(
+        color: Colors.red[900],
+        child: Text("Me, you, time and space.",
+          style: TextStyle(color: Colors.green),
+        ),
+      ),
+    ),
+  ),
+);
+
